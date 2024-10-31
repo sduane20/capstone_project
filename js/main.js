3d2d39e2a91f78ca553cc49b0ad441b777847df2
Stephen Duane <stephen.a.duane@gmail.com>
2024-10-31 12:42:03 -0400

    Added code for Google authentication.

---
+++ js/main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//import { initializeApp } from "firebase/app";
+//import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+
+// Your web app's Firebase configuration
+//const firebaseConfig = {
+  //apiKey: "AIzaSyBd_Bf-a5g8sNBD0zUXF6XTOKt3n6XD4vw",
+  //authDomain: "tranquil-thoughts.firebaseapp.com",
+ // projectId: "tranquil-thoughts",
+  //storageBucket: "tranquil-thoughts.appspot.com",
+  //messagingSenderId: "46951344026",
+ // appId: "1:46951344026:web:d6c924c18c9b52f5ce7c23"
+//};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+//const auth = getAuth(app);
+//const provider = new GoogleAuthProvider();
+
+
+//document.getElementById('Google').addEventListener('click', () => {
+    //signInWithPopup(auth, provider)
+    //.then((result) => {
+        //window.location.href = '../src/dashboard.html';
+    //})
+    //.catch((error) => {
+        //console.error('Error during Google Sign-in', error);
+    //});
+//});
+
+//https://tranquil-thoughts.firebaseapp.com/__/auth/handler
